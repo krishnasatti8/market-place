823ef5d0b4e08f24630c6f122d97c05fd8a597ae
krishnasatti8 <krishnasatti8@gmail.com>
2024-01-22 20:32:32 +0530

    Minor change

--- src/main/java/com/krishna/marketplace/utils/JwtUtil.java
+++ src/main/java/com/krishna/marketplace/utils/JwtUtil.java
@@ -28,7 +28,7 @@ public class JwtUtil {
 
 	private String createToken(Map<String, Object> claims, String username) {
 		return Jwts.builder().setClaims(claims).setSubject(username).setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 1))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
 				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
 	}
 
